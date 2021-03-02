f2ad9a8b431a8502c769156adbc8d0d9dd806a7d
vvv <vvv-nik@yandex.ru>
2021-03-02 06:42:58 +0300

    aded new algoritm QuickSort

--- SortQuick.py
+++ SortQuick.py
@@ -1,22 +1,49 @@
 import random
 
 a = []
-for j in range(1, 1000):
+for x in range(1, 1000):
     a.append(random.randint(1, 1000))
 
 print(a)
 
-def quick_sort(arr):
-    n = len(arr)
-    if n <= 1:
-        return arr
-    else:
-        pivot = random.choice(arr)
-        less = [x for x in arr if x < pivot]
-        greater_or_equal = [x for x in arr if x >= pivot]
-        return quick_sort(less) + quick_sort(greater_or_equal)
+# схема Tony Hoare
+def partition(nums, low, high):
+    # Мы выбираем средний элемент, в качестве опорного. Некоторые реализации выбирают
+    # первый элемент или последний элемент или вообще случайный элемент.
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+        if i >= j:
+            return j
+        # Если элемент в i (слева от оси) больше, чем
+        # элемент в J (справа от оси), то поменять их местами
+        nums[i], nums[j] = nums[j], nums[i]
+def quick_sort(nums):  
+    # Создаем вспомогательную рекурсивную функцию
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Это индекс после опорного элемента, по которому наши списки разделены
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+    _quick_sort(nums, 0, len(nums) - 1)
+# Проверяем, что все работает
 
-sort = quick_sort(a)
+
+import time
+start = time.time()
+quick_sort(a)
+
+
+end = time.time()
+print(end - start)
+
 
 print(a)
-
