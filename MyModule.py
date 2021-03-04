e5dbdc6aaa4f799d7547681a3e2e89ec9596f291
vvvnik <vvv-nik@yandex.ru>
2021-03-04 12:38:25 +0300

    Added MyModule Function

--- MyModule.py
+++ MyModule.py
@@ -1,9 +1,10 @@
 import os
-f=open("TestStr.txt",'r')
-a = f.read(len('TextStr.txt'))
-print(a)
-f.close()
 
+def MyReadTxtFile(NameFile):
+    f=open(NameFile,'r')
+    #print(f.read(len(NameFile)))
+    a=f.read(len(NameFile))
+    f.close()
+    return(a)
 
-
-
+print(MyReadTxtFile('TestStr.txt'))
