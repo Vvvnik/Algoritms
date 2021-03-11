11e7ed2ef2d376be4e3d8aa61f92d45af1c1e75e
vvvnik <vvv-nik@yandex.ru>
2021-03-11 10:19:30 +0300

    Change main.py

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
-import random
 import MyModule
 import time
 
-#     from MyModule import MyDanSort
-
 a = []
-
 buble = MyModule.MyDanSort(a)
-
-print(buble.get_quick_sort())
-print('Yes')
-del buble
-
-a = []
-for x in range(1, 300):
-    a.append(random.randint(601, 1000))
-
-for x in range(1, 300):
-    a.append(random.randint(301, 600))
-
-for x in range(1, 400):
-    a.append(random.randint(1, 300))
-print(a)
-
 start = time.time()
-
-MyModule.bubble_sort2(a)
+buble.get_quick_sort()
 end = time.time()
 print(end - start)
-
 print(a)
+del buble
