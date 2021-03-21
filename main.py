e3b379b2490c99dae5ba281f3c8868e1022292d1
vvvnik <vvv-nik@yandex.ru>
2021-03-21 20:49:41 +0300

    Change speed

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time
 
 
 # Функция декоратор расчета времени выполнения различных алгоритмов
-def decorator_calc_time(algoritmSort):
+def decorator_calc_time(algoritmsSort):
     start = time.time()
-    print(algoritmSort)
+    print(algoritmsSort)
     end = time.time()
     return end-start
 
 
 buble = MyModule.MyDanSort([])
-print(decorator_calc_time(buble.get_quick_sort()))
+print(round(decorator_calc_time(buble.getInsertionSort()), 4))
 del buble
