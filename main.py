827ba45cde932fb6362b79268ea133bae0ac7c2a
vvvnik <vvv-nik@yandex.ru>
2021-03-22 09:04:53 +0300

    Change speed with and without decorator

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-
 import MyModule
 import time
 
 
-
 # Функция декоратор расчета времени выполнения различных алгоритмов
 def decorator_calc_time(algoritmsSort):
-    start = time.time()
+    starter: float = time.time()
     print(algoritmsSort)
-    end = time.time()
-    return end-start
+    endear = time.time()
+    return endear-starter
+
 
 buble = MyModule.MyDanSort([])
-Decorator = 1
-start = time.time()
+Decorator = 0
 if Decorator:
-    x= decorator_calc_time(buble.getInsertionSort())
+    x = print(decorator_calc_time(buble.getInsertionSort()))
 else:
+    starter = time.time()
     print(buble.getInsertionSort())
-end = time.time()
-print(end-start)
+    endear = time.time()
+    print(endear-starter)
 del buble
-
-
