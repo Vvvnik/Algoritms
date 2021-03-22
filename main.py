28a73bff73eed20eca7142f729ac04d45c5ecd6e
vvvnik <vvv-nik@yandex.ru>
2021-03-22 09:13:03 +0300

    Change main.py with comment

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ import time
 
 # Функция декоратор расчета времени выполнения различных алгоритмов
 def decorator_calc_time(algoritmsSort):
-    starter: float = time.time()
+    starter = time.time()
     print(algoritmsSort)
     endear = time.time()
     return endear-starter
 
 
 buble = MyModule.MyDanSort([])
-Decorator = 0
+# Decorator =1 или = 0 или функция из класса вызывается через декоратор или напрямую (напрямую дольше, почему?)
+#   get_quick_sort в полтора раза отличается
+#   getInsertionSort в 57!
+#   причем c декоратором все функции работают быстро
+Decorator = 1
 if Decorator:
     x = print(decorator_calc_time(buble.getInsertionSort()))
 else:
